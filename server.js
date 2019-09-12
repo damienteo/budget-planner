4fb09a79ade2d8ccf4de33df7de2136d82d59460
damienteo <ksteo89@gmail.com>
2019-09-13 00:05:07 +0800

    Update server.js file,due to heroku issue when attempting to run build

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, "build")));
 
-app.get("*", function(req, res) {
+app.get("/ping", function(req, res) {
+  return res.send("pong");
+});
+
+app.get("/*", function(req, res) {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 
 app.listen(PORT);
-
-console.log("running build!");
